f588222f447d19e84e51f303ad36a982fdadfa9f
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:28:29 +0530

    Create sortarrayof0s,1,2s.cpp

---
+++ arrays/sortarrayof0s,1,2s.cpp
@@ -0,0 +1,19 @@
+void sortArray(vector<ll>& arr, ll n) {
+
+    ll low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
