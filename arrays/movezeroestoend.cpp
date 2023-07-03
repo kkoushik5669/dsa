b9382c0a31d4185b0caa40b2fb0c72b28376c792
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:12:05 +0530

    Create movezeroestoend.cpp

---
+++ arrays/movezeroestoend.cpp
@@ -0,0 +1,24 @@
+vector<ll> moveZeros(ll n, vector<ll> a) {
+    int j = -1;
+    //place the pointer j:
+    //this is a two pointer approach
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    //no non-zero elements:
+    if (j == -1) return a;
+
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
