fcd6533067001f609f9b5d47106c9e401bd6f1aa
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:03:10 +0530

    Update bubblesort.cpp

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -1,11 +1,11 @@
-void bubble_sort(vector<int>arr, int n) {
+void bubble_sort(vector<ll>arr, ll n) {
     // bubble sort
     //The heavier one goes to top in each iteration of i.
     //so basically sort the heavier element to right in each iteration.
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = 0; j <= i - 1; j++) {
+    for (ll i = n - 1; i >= 0; i--) {
+        for (ll j = 0; j <= i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                int temp = arr[j + 1];
+                ll temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
