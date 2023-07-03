c2eb979fe1d1aa097a0ecb0efc7a64c33d8ad356
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:32:46 +0530

    Create kadanesalgo.cpp

---
+++ arrays/kadanesalgo.cpp
@@ -0,0 +1,21 @@
+long long maxSubarraySum(vector<ll>arr, int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+
+    return maxi;
+}
