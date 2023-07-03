24cdf6bdeb720e71a718a96d215d7481cfef8c1c
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:03:42 +0530

    Update insertionsort.cpp

--- sorting/insertionsort.cpp
+++ sorting/insertionsort.cpp
@@ -1,11 +1,11 @@
-void insertion_sort(vector<int>arr, int n) {
+void insertion_sort(vector<ll>arr, ll n) {
     //sorts the array from left
     //first assumes the single element in the left is sorted.
     //then it procees right arranging each element sorted.
-    for (int i = 0; i <= n - 1; i++) {
-        int j = i;
+    for (ll i = 0; i <= n - 1; i++) {
+        ll j = i;
         while (j > 0 && arr[j - 1] > arr[j]) {
-            int temp = arr[j - 1];
+            ll temp = arr[j - 1];
             arr[j - 1] = arr[j];
             arr[j] = temp;
             j--;
