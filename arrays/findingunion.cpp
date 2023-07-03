777d6109bd0add845ecac8b7b29509b914aa571a
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-03 15:14:22 +0530

    Create findingunion.cpp

---
+++ arrays/findingunion.cpp
@@ -0,0 +1,30 @@
+vector < ll > FindUnion(vector<ll>arr1, vector<ll>arr2, ll n, ll m) {
+  ll i = 0, j = 0; // pointers
+  vector < ll > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
