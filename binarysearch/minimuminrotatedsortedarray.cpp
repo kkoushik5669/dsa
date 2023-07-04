b6d1d528788a079e0cb1f0f323d3e40c57c7e466
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-04 23:14:04 +0530

    Create minimuminrotatedsortedarray.cpp

---
+++ binarysearch/minimuminrotatedsortedarray.cpp
@@ -0,0 +1,32 @@
+ll findMin(vector<ll>& arr) {
+    ll low = 0, high = arr.size() - 1;
+    ll ans = INT_MAX;
+    while (low <= high) {
+        ll mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
