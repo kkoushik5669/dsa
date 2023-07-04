5eba6f5e4a7eed22a195fe00a88f386d27094b2e
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-04 23:17:40 +0530

    Create peakelement.cpp

---
+++ binarysearch/peakelement.cpp
@@ -0,0 +1,26 @@
+ll findPeakElement(vector<ll> &arr) {
+    ll n = arr.size(); //Size of array.
+
+    // Edge cases:
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    ll low = 1, high = n - 2;
+    while (low <= high) {
+        ll mid = (low + high) / 2;
+
+        //If arr[mid] is the peak:
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+
+        // If we are in the left:
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+        // If we are in the right:
+        // Or, arr[mid] is a common point:
+        else high = mid - 1;
+    }
+    // Dummy return statement
+    return -1;
+}
