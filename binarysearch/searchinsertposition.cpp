22d46270c7e67077b516ad91b7e55cc83a84d7d1
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-04 23:05:31 +0530

    Create searchinsertposition.cpp

---
+++ binarysearch/searchinsertposition.cpp
@@ -0,0 +1,19 @@
+ll searchInsert(vector<ll>& arr, ll x) {
+    ll n = arr.size(); // size of the array
+    ll low = 0, high = n - 1;
+    ll ans = n;
+
+    while (low <= high) {
+        ll mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
