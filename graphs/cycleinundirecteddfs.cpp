3f57bc67734759a8a7ac3ecfeb39aa6b207aa51f
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 17:01:47 +0530

    Create cycleinundirecteddfs.cpp

---
+++ graphs/cycleinundirecteddfs.cpp
@@ -0,0 +1,14 @@
+bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
+        vis[node] = 1; 
+        // visit adjacent nodes
+        for(auto adjacentNode: adj[node]) {
+            // unvisited adjacent node
+            if(!vis[adjacentNode]) {
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
+            }
+            // visited node but not a parent node
+            else if(adjacentNode != parent) return true; 
+        }
+        return false; 
+    }
