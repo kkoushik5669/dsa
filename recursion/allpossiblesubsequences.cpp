0c946af69a40afab726cc0c9aed1fc8f7cd896c7
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 15:53:44 +0530

    Create allpossiblesubsequences.cpp

---
+++ recursion/allpossiblesubsequences.cpp
@@ -0,0 +1,12 @@
+void solve(int i, string s, string &f) {
+	if (i == s.length()) {
+		cout << f << " ";
+		return;
+	}
+	//picking 
+	f = f + s[i];
+	solve(i + 1, s,  f);
+	//poping out while backtracking
+	f.pop_back();
+	solve(i + 1, s,  f);
+}
