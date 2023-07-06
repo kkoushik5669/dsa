fd907df085f15bd25a2e181cdce810e7c81e49c8
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 16:42:29 +0530

    Create findingxorupton.cpp

---
+++ bitmasks/findingxorupton.cpp
@@ -0,0 +1,15 @@
+ int func(int x){
+      
+      if(x%4==0){
+          return x;
+      }
+      if(x%4==1){
+          return 1;
+      }
+      if(x%4==2){
+          return x+1;
+      }
+      if(x%4==3){
+          return 0;
+      }
+  }
