69fdf9814ac4c1fb405188847d1cea7c72932eb8
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 15:58:20 +0530

    Create subsetsum2.cpp

---
+++ recursion/subsetsum2.cpp
@@ -0,0 +1,11 @@
+ void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
+      if (index == nums.size()) {
+        sort(ds.begin(), ds.end());
+        res.insert(ds);
+        return;
+      }
+      ds.push_back(nums[index]);
+      fun(nums, index + 1, ds, res);
+      ds.pop_back();
+      fun(nums, index + 1, ds, res);
+    }
