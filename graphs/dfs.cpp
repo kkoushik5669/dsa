007b162351dd0a3c8f24e871c1bff25705f4aa0a
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 16:57:23 +0530

    Create dfs.cpp

---
+++ graphs/dfs.cpp
@@ -0,0 +1,11 @@
+void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
+        vis[node] = 1; 
+        ls.push_back(node); 
+        // traverse all its neighbours
+        for(auto it : adj[node]) {
+            // if the neighbour is not visited
+            if(!vis[it]) {
+                dfs(it, adj, vis, ls); 
+            }
+        }
+    }
