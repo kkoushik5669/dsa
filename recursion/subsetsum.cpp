90d0a2c7f8642f3c48ad473b815096474c6f6017
kkoushik5669 <118590056+kkoushik5669@users.noreply.github.com>
2023-07-06 15:57:36 +0530

    Create subsetsum.cpp

---
+++ recursion/subsetsum.cpp
@@ -0,0 +1,10 @@
+ void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+      if (ind == n) {
+        ans.push_back(sum);
+        return;
+      }
+      //element is picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum + arr[ind]);
+      //element is not picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum);
+    }
